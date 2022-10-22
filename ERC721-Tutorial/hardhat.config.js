1d3f678aabfa2a49eafd502952ad18174ca762f9
oj-philz <sundayphilip069@gmail.com>
2022-10-22 17:12:04 +0100

    Revert "update hardhat.config.js"
    This reverts commit 8d559f9d6de74be0b80ec8032293d173d1ab8105.

--- ERC721-Tutorial/hardhat.config.js
+++ ERC721-Tutorial/hardhat.config.js
@@ -1,16 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
-require('dotenv').config()
-const GOERLI_URL = process.env.ALCHEMY_API_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-console.log(GOERLI_URL);
-console.log(PRIVATE_KEY);
+const { ALCHEMY_API_KEY : API_KEY, GOERLI_PRIVATE_KEY : PRIVATE_KEY } = require('../secret.json');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
   networks: {
     goerli: {
-      url: `${GOERLI_URL}`,
+      url: `https://eth-goerli.g.alchemy.com/v2/${API_KEY}`,
       accounts: [PRIVATE_KEY],
     },
   },
