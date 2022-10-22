5182c3458895ed9098f281f9915515c0ddc69910
oj-philz <sundayphilip069@gmail.com>
2022-10-22 17:20:18 +0100

    update hardhat.config.js

--- ERC721-Tutorial/hardhat.config.js
+++ ERC721-Tutorial/hardhat.config.js
@@ -1,12 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
-const { ALCHEMY_API_KEY : API_KEY, GOERLI_PRIVATE_KEY : PRIVATE_KEY } = require('../secret.json');
+require("dotenv").config()
+
+const URL = process.env.ALCHEMY_API_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
   networks: {
     goerli: {
-      url: `https://eth-goerli.g.alchemy.com/v2/${API_KEY}`,
+      url: URL,
       accounts: [PRIVATE_KEY],
     },
   },
