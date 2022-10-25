45975f20b9c045ed6c7f395c82fd593df9826e74
oj-philz <sundayphilip069@gmail.com>
2022-10-25 09:09:57 +0100

    update hardhat config

--- Buy_Me_a_Coffe_Dapp/hardhat.config.js
+++ Buy_Me_a_Coffe_Dapp/hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
+
+const API_URL = process.env.ALCHEMY_API_URL;
+const Api_Key = process.env.API_KEY
+const Private_Key = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: API_URL,
+      accounts: [Private_Key],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      goerli: Api_Key,
+    },
+  },
 };
