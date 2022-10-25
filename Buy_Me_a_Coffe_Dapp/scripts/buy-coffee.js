34b4cdf259ea713e3177000b17106ab2c20bba05
oj-philz <sundayphilip069@gmail.com>
2022-10-25 09:10:54 +0100

    create buy-coffee script

---
+++ Buy_Me_a_Coffe_Dapp/scripts/buy-coffee.js
@@ -0,0 +1,60 @@
+const hre = require("hardhat");
+
+async function getBalance(address) {
+  const balanceBigInt = await hre.ethers.provider.getBalance(address);
+  return hre.ethers.utils.formatEther(balanceBigInt);
+}
+
+async function printBalances(addresses) {
+  for (let i = 0; i < addresses.length; i++) {
+    console.log(`Address ${addresses[i]} balance: `, await getBalance(addresses[i]));
+  }
+}
+
+async function printMemos(memos) {
+  for (const memo of memos) {
+    const timestamp = memo.timestamp;
+    const tipper = memo.name;
+    const tipperAddress = memo.from;
+    const message = memo.message;
+    console.log(`At ${timestamp}, ${tipper} (${tipperAddress}) said: "${message}"`);
+  }
+}
+
+async function main() {
+  const [owner, tipper, tipper2, tipper3] = await hre.ethers.getSigners();
+
+  const BuyMeACoffee = await hre.ethers.getContractFactory("BuyMeACoffee");
+  const coffeeContract = await BuyMeACoffee.deploy();
+
+  await coffeeContract.deployed();
+  console.log(`Deployed contract address: ${coffeeContract.address}`);
+
+  const addresses = [owner.address, tipper.address, coffeeContract.address];
+  console.log("== start ==");
+  await printBalances(addresses);
+
+  const tip = {value: hre.ethers.utils.parseEther("1")};
+  await coffeeContract.connect(tipper).buyCoffee("Henry", "Awesome services!!", tip);
+  await coffeeContract.connect(tipper).buyCoffee("Sasa", "Awesome services!!", tip);
+  await coffeeContract.connect(tipper).buyCoffee("Chris", "Awesome services!!", tip);
+
+  console.log("== bought coffee ==");
+  await printBalances(addresses);
+
+  await coffeeContract.connect(owner).withdrawTips();
+
+  console.log("== withdraw tips ==");
+  await printBalances(addresses);
+
+  console.log("== memos ==");
+  const memos = await coffeeContract.getMemos();
+  printMemos(memos);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
